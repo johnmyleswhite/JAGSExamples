cae6b75606bfbc5bc1b3d38fb7b4725095e8b32f
John Myles White <jmw@johnmyleswhite.com>
2012-02-09 17:51:27 -0500

    Normal mean inference with uniform(0, 1) prior

---
+++ generators/normal_mean_strong_broken_prior.R
@@ -0,0 +1,10 @@
+set.seed(1)
+
+mu <- 25
+sigma <- 3
+
+x <- rnorm(100, mu, sigma)
+
+write.csv(data.frame(X = x),
+          file = file.path('data', 'normal_mean_strong_broken_prior.csv'),
+          row.names = FALSE)
