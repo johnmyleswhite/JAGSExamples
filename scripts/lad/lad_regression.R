826cf09040d832a0a4859e4ce9a940461b23f7ea
John Myles White <jmw@johnmyleswhite.com>
2012-02-09 18:20:42 -0500

    LAD + normal prior example with outliers

---
+++ scripts/lad/lad_regression.R
@@ -0,0 +1,32 @@
+library('rjags')
+
+df <- read.csv(file.path('data', 'lad', 'lad_regression.csv'))
+
+with(df, plot(X, Y))
+
+jags <- jags.model(file.path('bugs', 'lad', 'lad_regression.bugs'),
+                   data = list('x' = with(df, X),
+                               'y' = with(df, Y),
+                               'N' = nrow(df)),
+                   n.chains = 4,
+                   n.adapt = 1000)
+ 
+mcmc.samples <- coda.samples(jags,
+                        c('a', 'b', 'sigma'),
+                        1000)
+
+plot(mcmc.samples)
+
+summary(mcmc.samples)
+
+lm(Y ~ X, data = df)
+
+lad.coef <- summary(mcmc.samples)$statistics[c(2, 1), 1]
+ols.coef <- coef(lm(Y ~ X, data = df))
+
+library('ggplot2')
+
+ggplot(df, aes(x = X, y = Y)) +
+  geom_point() +
+  geom_abline(intercept = lad.coef[1], slope = lad.coef[2], color = "blue") +
+  geom_abline(intercept = ols.coef[1], slope = ols.coef[2], color = "green")
