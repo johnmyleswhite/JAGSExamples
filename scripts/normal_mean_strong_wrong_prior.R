7ee0f1c19c7e6c0c8559ceb40b096a4e7a82a04d
John Myles White <jmw@johnmyleswhite.com>
2012-02-09 17:49:50 -0500

    Normal mean inference with a strong, wrong prior

---
+++ scripts/normal_mean_strong_wrong_prior.R
@@ -0,0 +1,17 @@
+library('rjags')
+
+df <- read.csv(file.path('data', 'normal_mean_strong_wrong_prior.csv'))
+
+jags <- jags.model('bugs/normal_mean_strong_wrong_prior.bugs',
+                   data = list('x' = with(df, X),
+                               'N' = nrow(df)),
+                   n.chains = 4,
+                   n.adapt = 500)
+                   
+mcmc.samples <- coda.samples(jags,
+                             c('mu'),
+                             1000)
+
+plot(mcmc.samples)
+
+summary(mcmc.samples)
