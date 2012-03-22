61152f9eabcf94daf89e09921369e51e82a5141f
John Myles White <jmw@johnmyleswhite.com>
2012-03-22 10:07:55 -0400

    Revised LAD example

--- scripts/lad/lad_regression.R
+++ scripts/lad/lad_regression.R
@@ -1,10 +1,17 @@
+# Basic inference.
 library('rjags')
 
-df <- read.csv(file.path('data', 'lad', 'lad_regression.csv'))
+df <- read.csv(file.path('data',
+                         'lad',
+                         'lad_regression.csv'))
 
+png(file.path('graphs', 'lad', 'data_plot.png'))
 with(df, plot(X, Y))
+dev.off()
 
-jags <- jags.model(file.path('bugs', 'lad', 'lad_regression.bugs'),
+jags <- jags.model(file.path('bugs',
+                             'lad',
+                             'lad_regression.bugs'),
                    data = list('x' = with(df, X),
                                'y' = with(df, Y),
                                'N' = nrow(df)),
@@ -15,10 +22,13 @@ mcmc.samples <- coda.samples(jags,
                         c('a', 'b', 'sigma'),
                         1000)
 
+png(file.path('graphs', 'lad', 'plot1.png'))
 plot(mcmc.samples)
+dev.off()
 
 summary(mcmc.samples)
 
+# Compare with the OLS solution.
 lm(Y ~ X, data = df)
 
 lad.coef <- summary(mcmc.samples)$statistics[c(2, 1), 1]
@@ -28,5 +38,10 @@ library('ggplot2')
 
 ggplot(df, aes(x = X, y = Y)) +
   geom_point() +
-  geom_abline(intercept = lad.coef[1], slope = lad.coef[2], color = "blue") +
-  geom_abline(intercept = ols.coef[1], slope = ols.coef[2], color = "green")
+  geom_abline(intercept = lad.coef[1],
+              slope = lad.coef[2],
+              color = "blue") +
+  geom_abline(intercept = ols.coef[1],
+              slope = ols.coef[2],
+              color = "green")
+ggsave(file.path('graphs', 'lad', 'lad_vs_ols.png'))
