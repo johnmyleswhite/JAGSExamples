cd90d973113eab359253beea947ddbda3fa32dca
John Myles White <jmw@johnmyleswhite.com>
2012-04-26 08:38:16 -0400

    Updated slides

--- slides/likelihood_function.R
+++ slides/likelihood_function.R
@@ -6,11 +6,11 @@ df <- data.frame(theta = theta, L = l)
 
 ggplot(df, aes(x = theta, y = L)) +
   geom_line() +
-  opts(title = 'Likelihood Function after 9 Days of Rain and 1 Day of No Rain')
+  opts(title = 'Likelihood Function after 9 Mistaken Orders and 1 Correct Order')
 ggsave('likelihood_function.png')
 
 ggplot(df, aes(x = theta, y = L)) +
   geom_line() +
   geom_vline(xintercept = 0.9, color = 'blue') +
-  opts(title = 'Likelihood Function after 9 Days of Rain and 1 Day of No Rain')
+  opts(title = 'Likelihood Function after 9 Mistaken Orders and 1 Correct Order')
 ggsave('likelihood_function_mle.png')
